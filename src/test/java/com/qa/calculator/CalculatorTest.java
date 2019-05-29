f7616578c810f1fb9c0cf0751187ecb24d4e6e5b
lambord97 <dannylambord@hotmail.co.uk>
2019-05-29 12:09:20 +0100

    Fail

--- src/test/java/com/qa/calculator/CalculatorTest.java
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -8,8 +8,9 @@ public class CalculatorTest {
 	@Test
 	public void test() {
 		Calculator calc = new Calculator();
-		int actualValue = calc.add(2, 2);
-		Assert.assertEquals(4, actualValue);
+		int actualValue = calc.divide(2, 2);
+		Assert.assertEquals(1, actualValue);
 	}
+
 
 }
