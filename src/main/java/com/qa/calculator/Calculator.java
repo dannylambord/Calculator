1be25a79d4c650d54f03f34162122179ff6c41fd
lambord97 <dannylambord@hotmail.co.uk>
2019-05-29 14:02:56 +0100

    First

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -2,8 +2,8 @@ package com.qa.calculator;
 
 public class Calculator {
 
-	public int divide(int num1, int num2) {
-		return num1 / num2;
+	public int add(int num1, int num2) {
+		return num1 + num2;
 	}
 	
 	public int divide(int num1, int num2) {
